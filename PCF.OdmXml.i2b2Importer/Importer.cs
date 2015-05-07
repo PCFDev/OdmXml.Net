0e73fafbc4ca4f51e63cd475a777f2f452274194
IT Lackey <lackey_i@wustl.edu>
2015-05-07 12:49:54 -0500

    initial commit

---
+++ PCF.OdmXml.i2b2Importer/Importer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCF.OdmXml.i2b2Importer
+{
+    public class Importer
+    {
+        public async Task Import(string filePath)
+        {
+            var odm = ODM.Load(filePath);
+
+            
+
+        }
+    }
+}

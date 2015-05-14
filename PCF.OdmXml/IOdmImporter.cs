ed77b3727d63034c0973cd940d396db05cd1ba2a
IT Lackey <lackey_i@wustl.edu>
2015-05-14 00:52:51 -0500

    ugh here is the interface

---
+++ PCF.OdmXml/IOdmImporter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCF.OdmXml.i2b2Importer
+{
+
+    /// <summary>
+    /// An interface for ODMXML importers
+    /// </summary>
+    public interface IOdmImporter
+    {
+        /// <summary>
+        /// Takes a populated ODM model and inserts the data into an i2b2 database
+        /// </summary>
+        /// <param name="odm">Fully populated ODMXML model</param>
+        /// <returns></returns>
+        Task ImportAsync(ODM odm);
+    }
+}

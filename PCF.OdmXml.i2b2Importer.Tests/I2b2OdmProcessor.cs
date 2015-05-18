65f42fce8ef9d50e879532474d60b6ea0a6fe197
Aaron-P <papp_a@kids.wustl.edu>
2015-05-18 10:05:11 -0500

    Add unit test project for testing the importer.

---
+++ PCF.OdmXml.i2b2Importer.Tests/I2b2OdmProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using PCF.OdmXml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PCF.OdmXml.i2b2Importer.Tests
+{
+    [TestClass]
+    public class I2b2OdmProcessor
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ODM odm;
+            Exception exception;
+            Assert.IsTrue(ODM.Load(@"Samples\odm130.xml", out odm, out exception));
+            Assert.IsNotNull(odm);
+            Assert.IsNull(exception);
+
+
+        }
+    }
+}
